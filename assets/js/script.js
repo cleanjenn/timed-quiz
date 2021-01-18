393cf10bf94f7ffddca9a73fc3a4ee8cdab133b0
jennifer gomez <jennifer23gomez7@gmail.com>
2021-01-17 17:31:37 -0800

    timer and alignment adjusted

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//start by creating DOM elements
+//variables for questions and answers
+//vaariables for timer 
+//variables for answers right and wrong
+//variables for score
+//functions for timer, random question selection, quiz start
+//functions for high score and local storage
+//finish off with event listeners 
